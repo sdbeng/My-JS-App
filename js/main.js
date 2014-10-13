ab8f98b8a6438f4a879c73b8e02275162bb36472
Sergio Barrera <sdbeng@gmail.com>
2014-10-13 09:57:08 -0700

    Restore pure JS

--- js/main.js
+++ js/main.js
@@ -11,29 +11,31 @@ form.form_Init.addEventListener("submit", eventStartRegHandler);
 
 function eventStartRegHandler(event) {
     event.preventDefault();
+
+    event.preventDefault();
+    alert("redirecting user...");
+    window.location = 'registration.html';
     
-	xmlhttp = new XMLHttpRequest();
+	// xmlhttp = new XMLHttpRequest();
 
-	xmlhttp.onreadystatechange = function() {
-        if(xmlhttp.readyState == 4) {
-           if(xmlhttp.status != 200) {
-              alert('There was an error');
-           } else {
-           		var response = JSON.parse(xmlhttp.responseText);
-           		if(response.status!='true') {
-           			alert('Email taken');
-           		} else {
+	// xmlhttp.onreadystatechange = function() {
+ //        if(xmlhttp.readyState == 4) {
+ //           if(xmlhttp.status != 200) {
+ //              alert('There was an error');
+ //           } else {
+ //           		var response = JSON.parse(xmlhttp.responseText);
+ //           		if(response.status!='true') {
+ //           			alert('Email taken');
+ //           		} else {
 
-           		}
-           }
-        }
-    }
+ //           		}
+ //           }
+ //        }
+ //    }
 
-    xmlhttp.open('GET', 'helper.php?action=checkEmailAvailability&email='+encodeURIcomponent(form.email), true);
-    xmlhttp.send();
-	//event.preventDefault();
-	//alert("redirecting user...");
-	//window.location = 'registration.html';
+ //    xmlhttp.open('GET', 'helper.php?action=checkEmailAvailability&email='+encodeURIcomponent(form.email), true);
+ //    xmlhttp.send();
+	
 };
 
-var testobj = {};
+// var testobj = {};
